60483777c1bbc33118bdba58d381f3f07d39012b
Farhan <>
2019-04-17 23:50:02 +0200

    added code for relay

--- chilli.py
+++ chilli.py
@@ -2,13 +2,16 @@
 import grovepi
 import math
 import socket
+import datetime as dt
 
 from grove_rgb_lcd import *
 
 dht_sensor = 3  # The Temp & Hum Sensor goes on digital port 3.
+relay = 4 # Connect the Grove Relay to digital port D4
 light_sensor = 0  # Grove Light Sensor to analog port A0
 
 grovepi.pinMode(light_sensor, "INPUT")
+grovepi.pinMode(relay, "OUTPUT")
 
 # ugly hack, but get the ip and shows on startup on screen.
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -19,6 +22,13 @@ s.close()
 i = 0
 while True:
     try:
+        if True:
+            # switch on
+            grovepi.digitalWrite(relay, 1)
+        else:
+            # switch off
+            grovepi.digitalWrite(relay, 0)
+
         # blue=0, white=1
         [temp, humidity] = grovepi.dht(dht_sensor, 0)
         setRGB(32, 128, 32)
@@ -45,5 +55,9 @@ while True:
         print(firstRow + "\n" + secondRow)
         time.sleep(1.00)
 
+    except KeyboardInterrupt:
+        grovepi.digitalWrite(relay, 0)
+        print("exit")
+        break
     except IOError:
-        print ("Error")
+        print("IO Error")
