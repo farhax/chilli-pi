6a36d27351218af9c216352928b11233411a9e7d
Farhan <>
2019-05-24 11:10:46 +0200

    added update file for checking for new code

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-echo "Checking for update from git"
-echo "git -C $SCRIPTPATH pull"
-git -C "$SCRIPTPATH" pull
-
-python3 "$SCRIPTPATH"/chilli.py
-
